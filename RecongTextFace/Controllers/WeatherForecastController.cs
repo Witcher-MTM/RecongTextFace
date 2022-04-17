99724bda94fdcd046b1866753d8432a3732d3c28
Witcher-MTM <phantomit228@gmail.com>
2022-04-17 15:23:03 +0300

    Add project files.

---
+++ RecongTextFace/Controllers/WeatherForecastController.cs
@@ -0,0 +1,38 @@
+﻿using Google.Cloud.Vision.V1;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RecongTextFace.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string getText(string url)
+        {
+            Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", "info.json");
+            string info = String.Empty;
+            ImageAnnotatorClient client = ImageAnnotatorClient.Create();
+            IReadOnlyList<EntityAnnotation> textAnnotations = client.DetectText(Image.FromUri(url));
+            foreach (EntityAnnotation text in textAnnotations)
+            {
+                info += $"{text.Description}\n";
+            }
+            return info;
+        }
+       
+    }
+}
